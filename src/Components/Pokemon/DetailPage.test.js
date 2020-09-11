40620a5c0188c238f2372ee46a8b0a741f246493
Squirtle <tienhoang.pham@bc.fi>
2020-09-11 14:39:49 +0300

    Add Mock API response with dummy data

--- src/Components/Pokemon/DetailPage.test.js
+++ src/Components/Pokemon/DetailPage.test.js
@@ -3,14 +3,21 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import DetailPage from './DetailPage'
 
-test('<DetailPage> should receive the id props', () => {
+
+
+test('<DetailPage> should receive the id props', (done) => {
     // when something beyond this point, makes a fetch call
     // mock the functionality of fetch, makes no API call, but return Promise.resolve()
     // as the API response
+    var pokemon = {
+        sprites: { front_default: 'https://contestimg.wish.com/api/webimage/5eb4383af6961742f86aac6f-large.jpg?cache_buster=941f9857e28c675687d2ce1d36e3aa8e'},
+        name: 'pika'
+    }
+
     jest.spyOn(global, 'fetch').mockImplementation(function(){
         return Promise.resolve({
             ok: true,
-            json: () => {}
+            json: () => Promise.resolve(pokemon)
         })
     })
 
@@ -25,9 +32,9 @@ test('<DetailPage> should receive the id props', () => {
     // we expect the fetch call to be called with the following parameter
     expect(global.fetch).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/25')
 
-    // process.nextTick(() => {
-    //     wrap.update()
-    //     console.log(wrap.debug())
-    //     done()
-    // })       
+    process.nextTick(() => {
+        // wrap.update()
+        expect(wrap.find('.card-title').text()).toBe('pika')
+        done()
+    })       
 });
